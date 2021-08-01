9ef1cb4202cb13cd79f362f1c9290c614c72366f
fdarame <57066188+fdarame@users.noreply.github.com>
2021-08-01 14:15:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-    function check(){
-	 var count = 99;
-	 var word="bottles";
-
-      //while (count>1){
-	 for( i = 99; i >0;i=i-1){
-
-	    (count+" " +word+" of beer on the wall"+);
-		document.getElementById("placeholder").innerHTML="99 bottles of beer on the wall,";
-		(count+" " +word+ " of beer,");
-		document.getElementById("placeholder").innerHTML="99 bottles of beer";
-		("Take one down, pass it around,");
-		document.getElementById("placeholder").innerHTML="Take one down, pass it around,";
-		 count=count-1;
-	 
-			if (count>1){
-			 (count+" " +word+" of beer on the wall");
-			 document.getElementById("placeholder").innerHTML="98 bottles of beer on the wall,"
-			}
-		 
-		 else if (count==1){
-			 (count+" " +word+" of beer on the wall");
-			 (count+" " +word+" of beer on the wall");
-			 (count+" " +word+ " of beer,");
-			("Take one down, pass it around,");
-		 }
-	       
-		 
-	    else (count==0){
-		 ("No more+" +word+" of beer on the wall");
-	      document.getElementById("placeholder").innerHTML="No more bottles of beer on the wall,"
- 
-        }
-	 }
+alert("Click here to generate a random numnber");
+document.getElementById("main").innerHTML =
+Math.floor(Math.random() * 10);
